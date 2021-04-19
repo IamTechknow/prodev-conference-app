1a7020951126233614da418acf3a65b123baf42d
Edmond Lee <techhexium@gmail.com>
2021-04-18 22:58:25 -0700

    Add circuit breaker support for events list Triggered when request timeout after 2 seconds, easily done by turning off the conference microservice

--- frontend/src/components/events/events.jsx
+++ frontend/src/components/events/events.jsx
@@ -1,26 +1,51 @@
 import React, { Component } from "react";
+import { toast } from "react-toastify";
+import CircuitBreaker from 'opossum';
 import Spinner from '../spinner';
 import NoEvents from './noEvents';
 import EventsList from './eventsList';
 import { AuthorizedPage } from '../../services/authService';
 import { getAll } from '../../services/eventService';
 
-
+const circuitBreakerOptions = {
+  timeout: 2000, // If our function takes longer than 2 seconds, trigger a failure
+  errorThresholdPercentage: 1, // Trip circuit if any error at all
+  resetTimeout: 10000 // After 10 seconds, try again.
+};
 
 class Events extends Component {
   constructor(params) {
     super(params);
     this.state = { loading: true, events: [], errors: [], showForm: false };
+    this.breaker = new CircuitBreaker(async () => await getAll(), circuitBreakerOptions);
   }
 
   async componentDidMount() {
-    const result = await getAll();
-    if (result.succeeded) {
-      this.setState({ events: result.events });
-    } else {
-      this.setState({ errors: result.errors });
-    }
-    this.setState({ loading: false });
+    this.breaker.fallback(() => {
+      this.setState({ errors: ["Sorry, circuit breaker tripped when loading events!"] });
+    });
+    this.breaker.on('success', result => {
+      if (result.succeeded) {
+        this.setState({ events: result.events });
+      } else {
+        this.setState({ errors: result.errors });
+      }
+      this.setState({ loading: false });
+    });
+    this.breaker.on('timeout', () => {
+      console.log("Circuit breaker is open due to timeout");
+      toast.error('Sorry, circuit breaker tripped when loading events!');
+    });
+    this.breaker.on('halfOpen', () => {
+      console.log("Circuit breaker is now half open");
+      this.setState({ loading: true });
+      this.loadEvents();
+    });
+    this.loadEvents();
+  }
+
+  loadEvents() {
+    this.breaker.fire().catch(e => console.error(e));
   }
 
   showForm = e => {
@@ -37,9 +62,9 @@ class Events extends Component {
   render() {
     return (
       <AuthorizedPage redirectToPath="/login">
-        { this.state.loading ? 
+        { this.state.loading ?
           <div className="text-center"><Spinner /></div> :
-          ( this.state.events.length === 0 ?
+          (this.state.events.length === 0 ?
             <NoEvents show={this.state.showForm} showForm={this.showForm} hideForm={this.hideForm} /> :
             <EventsList events={this.state.events} show={this.state.showForm} showForm={this.showForm} hideForm={this.hideForm} />
           )
